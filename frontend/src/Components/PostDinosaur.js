95b1e3293568f3987d66ac7eba02d0965426ad52
Rhmuse <rhett.muse123@gmail.com>
2023-11-26 17:20:15 -0600

    Add viewAllDino, postDino pages

---
+++ frontend/src/Components/PostDinosaur.js
@@ -0,0 +1,12 @@
+const PostDinosaur = () => {
+    return <iframe
+        name="New Dinosaur"
+        title="New Dinosaur"
+        src="https://c2hbt072.caspio.com/dp/32d4d00058a87813342f46d08387"
+        className='caspio-iframe'
+        style={{ width: "100%", height: "80vh" }}>
+        Sorry, but your browser does not support frames.
+    </iframe>
+}
+
+export default PostDinosaur;
---
+++ frontend/src/Components/ViewAllDinosaurs.js
@@ -0,0 +1,13 @@
+const ViewAllDinosaurs = () => {
+    return <iframe
+        name="View All Dinosaurs"
+        title="View All Dinosaurs"
+        src="https://c2hbt072.caspio.com/dp/32d4d000de73e1e99fc14f43aba4"
+        className='caspio-iframe'
+        style={{ width: "100%", height: "80vh" }}>
+        Sorry, but your browser does not support frames.
+    </iframe>
+
+}
+
+export default ViewAllDinosaurs;
